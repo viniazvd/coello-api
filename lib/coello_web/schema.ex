4049c713847630e844f5c89baaa83c70c55f7ecb
Vinicius Azevedo <viniazvd@gmail.com>
2019-10-09 15:54:45 -0300

    :zap: add user list query

--- lib/coello_web/schema.ex
+++ lib/coello_web/schema.ex
@@ -1,13 +1,19 @@
 defmodule CoelloWeb.Schema do
   use Absinthe.Schema
 
+  alias CoelloWeb.Graphql.Resolvers
+
   # import types
-  import_types(CoelloWeb.Graphql.Types)
+  import_types(CoelloWeb.Schema.Types.Index)
 
   query do
-
+    @docs "user list"
+    field :users, list_of(:user_type) do
+      resolve(&Resolvers.UserResolver.user/3)
+    end
   end
 
   # mutation do end
+
   # subscription do end
 end
