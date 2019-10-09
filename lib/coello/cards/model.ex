cbda7f3bec2fd9e79864ec92236e90c958254e5c
Vinicius Azevedo <viniazvd@gmail.com>
2019-10-09 15:53:46 -0300

    :tada: :moved: init

---
+++ lib/coello/cards/model.ex
@@ -0,0 +1,21 @@
+defmodule Coello.Cards.Model do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  @primary_key {:id, :binary_id, autogenerate: true}
+  @foreign_key_type :binary_id
+  schema "cards" do
+    field :description, :string
+    field :order, :integer
+    field :title, :string
+
+    timestamps()
+  end
+
+  @doc false
+  def changeset(card, attrs) do
+    card
+    |> cast(attrs, [:order, :title, :description])
+    |> validate_required([:order, :title, :description])
+  end
+end
