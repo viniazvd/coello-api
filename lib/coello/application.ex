2514984d846d859775585f13e9c6677e004fe89d
Vinicius Azevedo <vinicius.azevedo@convenia.com.br>
2019-10-10 15:50:20 -0300

    :fire: remove comments

--- lib/coello/application.ex
+++ lib/coello/application.ex
@@ -1,8 +1,4 @@
 defmodule Coello.Application do
-  # See https://hexdocs.pm/elixir/Application.html
-  # for more information on OTP Applications
-  @moduledoc false
-
   use Application
 
   def start(_type, _args) do
