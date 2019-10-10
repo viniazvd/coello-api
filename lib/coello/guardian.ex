cf80318e2e510051abf72290efa45ccb4e5ee7d7
Vinicius Azevedo <viniazvd@gmail.com>
2019-10-10 00:25:56 -0300

    :hammer: refactor #1

--- lib/coello/guardian.ex
+++ lib/coello/guardian.ex
@@ -1,25 +1,24 @@
 defmodule Coello.Guardian do
   use Guardian, otp_app: :coello
   alias Coello.Users.Controller, as: UserController
+  alias Coello.Users.Model, as: UserModel
 
-  def subject_for_token(user, _claims) do
-    # You can use any value for the subject of your token but
-    # it should be useful in retrieving the resource later, see
-    # how it being used on `resource_from_claims/1` function.
-    # A unique `id` is a good subject, a non-unique email address
-    # is a poor subject.
-    sub = to_string(user.id)
-    {:ok, sub}
+  def subject_for_token(%UserModel{} = user, _claims) do
+    {:ok, to_string(user.id)}
   end
 
-  def resource_from_claims(claims) do
-    # Here we'll look up our resource from the claims, the subject can be
-    # found in the `"sub"` key. In `above subject_for_token/2` we returned
-    # the resource id so here we'll rely on that to look it up.
-    user =
-      claims["sub"]
-      |> UserController.get_user!()
+  def subject_for_token(_, _) do
+    {:error, :reason_for_error}
+  end
 
-    {:ok, user}
+  def resource_from_claims(%{"sub" => id}) do
+    case UserController.get_user!(id) do
+      nil -> {:error, :resource_not_found}
+      user -> {:ok, user}
+    end
+  end
+
+  def resource_from_claims(_claims) do
+    {:error, :reason_for_error}
   end
 end
