e79e42a569e2496a8030d67434ff9eb7e269f8e6
Vinicius Azevedo <viniazvd@gmail.com>
2019-10-09 21:43:52 -0300

    :sparkles: add auth type

--- lib/coello_web/graphql/types/index.ex
+++ lib/coello_web/graphql/types/index.ex
@@ -1,5 +1,8 @@
 defmodule CoelloWeb.Graphql.Types.Index do
   use Absinthe.Schema.Notation
 
+  # TODO: create alias
+
   import_types(CoelloWeb.Graphql.Types.UserType)
+  import_types(CoelloWeb.Graphql.Types.AuthType)
 end
