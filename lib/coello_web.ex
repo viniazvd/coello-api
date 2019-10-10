a57b5155ec0d392559df203b5c526d49467879d9
Vinicius Azevedo <vinicius.azevedo@convenia.com.br>
2019-10-10 15:46:12 -0300

    :fire: remove comments

--- lib/coello_web.ex
+++ lib/coello_web.ex
@@ -1,21 +1,4 @@
 defmodule CoelloWeb do
-  @moduledoc """
-  The entrypoint for defining your web interface, such
-  as controllers, channels and so on.
-
-  This can be used in your application as:
-
-      use CoelloWeb, :controller
-
-  The definitions below will be executed for every
-  controller, etc, so keep them short and clean, focused
-  on imports, uses and aliases.
-
-  Do NOT define functions inside the quoted expressions
-  below. Instead, define any helper function in modules
-  and import those modules here.
-  """
-
   def controller do
     quote do
       use Phoenix.Controller, namespace: CoelloWeb
@@ -39,9 +22,6 @@ defmodule CoelloWeb do
     end
   end
 
-  @doc """
-  When used, dispatch to the appropriate controller/etc.
-  """
   defmacro __using__(which) when is_atom(which) do
     apply(__MODULE__, which, [])
   end
